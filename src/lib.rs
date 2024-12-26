a8ab835b490e096eb334a35cbd6845de63ac935a
Mahmoud <oss@wiseai.dev>
2024-12-26 17:36:01 +0200

    chore: document features && update logo url

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc(
-    html_logo_url = "https://github.com/opensass/accordion-rs/blob/main/assets/logo.webp",
-    html_favicon_url = "https://github.com/opensass/accordion-rs/blob/main/assets/favicon.png"
+    html_logo_url = "https://raw.githubusercontent.com/opensass/accordion-rs/refs/heads/main/assets/logo.webp",
+    html_favicon_url = "https://raw.githubusercontent.com/opensass/accordion-rs/refs/heads/main/assets/favicon.png"
 )]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
